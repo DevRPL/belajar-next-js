a2eed2bd990c564078b1d6c6fc59e990556ec012
Muhamad Jamil Firdaus <muhamadjamil668@gmail.com>
2023-12-18 13:41:43 +0700

    Pages Router : 3.Link & Navigation

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const LoginPage = () => {
+    const {push} = useRouter(); 
+    const HandlerLogin = () => {
+        push("/product");
+    } 
+
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <button onClick={HandlerLogin}>Login</button>
+            <p>?Belum punya akun ? Registrasi <Link href={"/auth/register"}>Disini</Link></p>
+        </div>
+    )
+}
+
+export default LoginPage;
